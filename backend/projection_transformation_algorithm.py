dd1fe0203c9acb5cd44bf7f22c2995223d27d45a
Fanxin-Yang <fanxin.yang@tum.de>
2022-07-19 10:40:52 +0200

    pm4py change accoeding to API document

--- backend/projection_transformation_algorithm.py
+++ backend/projection_transformation_algorithm.py
@@ -8,9 +8,10 @@
 import os
 import pandas
 from flask import request, current_app
-from pm4py.objects.log.importer.xes import importer as xes_importer
-from pm4py.objects.conversion.log import converter as log_converter
-from pm4py.algo.filtering.log.attributes import attributes_filter
+import pm4py
+# from pm4py.objects.log.importer.xes import importer as xes_importer
+# from pm4py.objects.conversion.log import converter as log_converter
+# from pm4py.algo.filtering.log.attributes import attributes_filter
 
 
 # def convert(type):
@@ -42,7 +43,8 @@ def get_attributes(filename):
     input_path = exist_file(filename)
     if input_path == False:
         return "No file found.", 404
-    log = xes_importer.apply(input_path)
+    # log = xes_importer.apply(input_path)
+    log = pm4py.read_xes(input_path)
     # variant = xes_importer.Variants.ITERPARSE
     # parameters = {variant.value.Parameters.TIMESTAMP_SORT: True}
     # log = xes_importer.apply(
@@ -68,17 +70,21 @@ def projection_transformation(filename, att):
     if not input_path:
         return "No file found.", 404
     # check if att is contained in the file?
-    log = xes_importer.apply(input_path)
+    log = pm4py.read_xes(input_path)  
     # values: all attribute values of selected attribute and the number each of them occur
-    attValues = attributes_filter.get_attribute_values(log, att)
+    # attValues = attributes_filter.get_attribute_values(log, att)
+    attValues = pm4py.get_event_attribute_values(log, att)
 
     if not os.path.exists("outputs/" + filename):
         os.makedirs("outputs/" + filename)
     for key in attValues.keys():
-        filterLog = attributes_filter.apply_events(log, [key], parameters={
-                                                   attributes_filter.Parameters.ATTRIBUTE_KEY: "org:resource", attributes_filter.Parameters.POSITIVE: True})
-        dataframe = log_converter.apply(
-            filterLog, variant=log_converter.Variants.TO_DATA_FRAME)
+        print(key)
+        # filterLog = attributes_filter.apply_events(log, [key], parameters={
+        #                                            attributes_filter.Parameters.ACTIVITY_KEY: "org:resource", attributes_filter.Parameters.POSITIVE: True})
+        filterLog = pm4py.filter_event_attribute_values(log, att, [key], level="case", retain=True)
+        # dataframe = log_converter.apply(
+        #     filterLog, variant=log_converter.Variants.TO_DATA_FRAME)
+        dataframe = pm4py.convert_to_dataframe(filterLog)
         if not os.path.exists("outputs/" + filename + "/" + str(key)):
             os.makedirs("outputs/" + filename + "/" + str(key))
         output_path = os.path.join(

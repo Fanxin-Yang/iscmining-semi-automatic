6c5fcf3f4448bcbb939e152d40dfa91b7f44df32
Fanxin-Yang <fanxin.yang@tum.de>
2022-08-04 17:46:48 +0200

    add create_app()

--- backend/app.py
+++ backend/app.py
@@ -8,142 +8,145 @@ import graphviz
 import projection_transformation_algorithm
 import discovery_algorithm
 
-UPLOAD_FOLDER = "./uploads"
-OUTPUT_FOLDER = "./outputs"
-GRAPH_FOLDER = "./processmodels"
-RESULTS_FOLDER = "./results"
-ALLOWED_EXTENSIONS = {"xes"}
-app = Flask(__name__)
-# The secret key will change after each start, and incalidate any signed cookies.
-# So for now just set a random secret key
-# secret = secrets.token_urlsafe(32)
-# app.secret_key = secret
-app.secret_key = "secret_key#!"
+def create_app():
+    UPLOAD_FOLDER = "./uploads"
+    OUTPUT_FOLDER = "./outputs"
+    GRAPH_FOLDER = "./processmodels"
+    RESULTS_FOLDER = "./results"
+    ALLOWED_EXTENSIONS = {"xes"}
+    app = Flask(__name__)
+    # The secret key will change after each start, and incalidate any signed cookies.
+    # So for now just set a random secret key
+    # secret = secrets.token_urlsafe(32)
+    # app.secret_key = secret
+    app.secret_key = "secret_key#!"
 
-app.config.from_object(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
-app.config['GRAPH_FOLDER'] = GRAPH_FOLDER
-app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
-CORS(app, resources={r"/*": {'origins': "*"}})
+    app.config.from_object(__name__)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+    app.config['GRAPH_FOLDER'] = GRAPH_FOLDER
+    app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
+    CORS(app, resources={r"/*": {'origins': "*"}})
 
-# Centralized URL Map
-# https://flask.palletsprojects.com/en/1.1.x/patterns/lazyloading/converting-to-centralized-url-map
-app.add_url_rule('/projection_transformation/<filename>',
-                 view_func=projection_transformation_algorithm.get_attributes)
-app.add_url_rule(
-    '/projection_transformation/<filename>/<att>',
-    view_func=projection_transformation_algorithm.projection_transformation)
-app.add_url_rule('/discovery/<filename>/<csv>',
-                 view_func=discovery_algorithm.get_events)
-app.add_url_rule('/discovery/<filename>/<csv>/<int:eventIndex>',
-                 methods=['DELETE'],
-                 view_func=discovery_algorithm.delete_event)
-app.add_url_rule('/discovery/<filename>/<csv>/<string:level>',
-                 methods=['PUT'],
-                 view_func=discovery_algorithm.adapt_timestamps)
-app.add_url_rule('/discovery', view_func=discovery_algorithm.get_algorithms)
-app.add_url_rule('/discovery/<filename>/<csv>/<string:alg>',
-                 methods=['GET'],
-                 view_func=discovery_algorithm.appy_algorithm)
-app.add_url_rule('/decisiontree/<filename>/<csv>',
-                 methods=['GET'],
-                 view_func=discovery_algorithm.get_decisiontree)
-
-
-@app.route('/', methods=['GET'])
-def greetings():
-    return ('Main Page')
+    # Centralized URL Map
+    # https://flask.palletsprojects.com/en/1.1.x/patterns/lazyloading/converting-to-centralized-url-map
+    app.add_url_rule('/projection_transformation/<filename>',
+                    view_func=projection_transformation_algorithm.get_attributes)
+    app.add_url_rule(
+        '/projection_transformation/<filename>/<att>',
+        view_func=projection_transformation_algorithm.projection_transformation)
+    app.add_url_rule('/discovery/<filename>/<csv>',
+                    view_func=discovery_algorithm.get_events)
+    app.add_url_rule('/discovery/<filename>/<csv>/<int:eventIndex>',
+                    methods=['DELETE'],
+                    view_func=discovery_algorithm.delete_event)
+    app.add_url_rule('/discovery/<filename>/<csv>/<string:level>',
+                    methods=['PUT'],
+                    view_func=discovery_algorithm.adapt_timestamps)
+    app.add_url_rule('/discovery', view_func=discovery_algorithm.get_algorithms)
+    app.add_url_rule('/discovery/<filename>/<csv>/<string:alg>',
+                    methods=['GET'],
+                    view_func=discovery_algorithm.appy_algorithm)
+    app.add_url_rule('/decisiontree/<filename>/<csv>',
+                    methods=['GET'],
+                    view_func=discovery_algorithm.get_decisiontree)
 
 
-@app.route('/processmodels/<name>', methods=['GET'])
-def get_processmodels(name):
-    return send_from_directory(app.config['GRAPH_FOLDER'], name)
-    return send_from_directory(app.config['GRAPH_FOLDER'],
-                               name,
-                               as_attachment=True)
+    @app.route('/', methods=['GET'])
+    def greetings():
+        return ('Main Page')
 
 
-# check if the filname inculde "." and the suffix of it is allowed
+    @app.route('/processmodels/<name>', methods=['GET'])
+    def get_processmodels(name):
+        return send_from_directory(app.config['GRAPH_FOLDER'], name)
+        return send_from_directory(app.config['GRAPH_FOLDER'],
+                                name,
+                                as_attachment=True)
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    # check if the filname inculde "." and the suffix of it is allowed
 
 
-def mining_process_model(file_path):
-    log = xes_importer.apply(file_path)
-    # dataframe = pm4py.convert_to_dataframe(log)
-    # dataframe.to_csv('loan_process.csv')
-    net, initial_marking, final_marking = pm4py.discover_petri_net_inductive(
-        log)
-    return net, initial_marking, final_marking
+    def allowed_file(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/pm4pytest', methods=['GET'])
-def pm4pytest():
-    filename = 'loan_process.xes'
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    if not os.path.exists(app.config['GRAPH_FOLDER']):
-                os.makedirs(app.config['GRAPH_FOLDER'])
-    pnml_path = os.path.join(app.config['GRAPH_FOLDER'],
-                             filename.rsplit('.', 1)[0].lower() + ".pnml")
-    # (must be one of ['bmp', 'canon', 'cgimage', 'cmap', 'cmapx', 'cmapx_np', 'dot', 'dot_json', 'eps', 'exr', 'fig', 'gd', 'gd2', 'gif', 'gtk', 'gv', 'ico', 'imap', 'imap_np', 'ismap', 'jp2', 'jpe', 'jpeg', 'jpg', 'json', 'json0', 'pct', 'pdf', 'pic', 'pict', 'plain', 'plain-ext', 'png', 'pov', 'ps', 'ps2', 'psd', 'sgi', 'svg', 'svgz', 'tga', 'tif', 'tiff', 'tk', 'vml', 'vmlz', 'vrml', 'wbmp', 'webp', 'x11', 'xdot', 'xdot1.2', 'xdot1.4', 'xdot_json', 'xlib'])
-    vis_path = os.path.join(app.config['GRAPH_FOLDER'],
-                            filename.rsplit('.', 1)[0].lower() + ".png")
-    net, initial_marking, final_marking = mining_process_model(file_path)
-    pm4py.write_petri_net(net, initial_marking, final_marking, pnml_path)
-    pm4py.save_vis_petri_net(net, initial_marking, final_marking, vis_path)
-    # graphviz.render('dot', 'png', vis_path).replace('\\', '/')
-    return send_from_directory(app.config['GRAPH_FOLDER'], 'loan_process.png')
+    def mining_process_model(file_path):
+        log = xes_importer.apply(file_path)
+        # dataframe = pm4py.convert_to_dataframe(log)
+        # dataframe.to_csv('loan_process.csv')
+        net, initial_marking, final_marking = pm4py.discover_petri_net_inductive(
+            log)
+        return net, initial_marking, final_marking
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return "No file uploaded. Please select a file.", 400
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return "No file is selected.", 400
-            # return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            if not os.path.exists(app.config['GRAPH_FOLDER']):
-                os.makedirs(app.config['GRAPH_FOLDER'])
-            pnml_path = os.path.join(app.config['GRAPH_FOLDER'],
-                             filename.rsplit('.', 1)[0].lower() + ".pnml")
-             # (must be one of ['bmp', 'canon', 'cgimage', 'cmap', 'cmapx', 'cmapx_np', 'dot', 'dot_json', 'eps', 'exr', 'fig', 'gd', 'gd2', 'gif', 'gtk', 'gv', 'ico', 'imap', 'imap_np', 'ismap', 'jp2', 'jpe', 'jpeg', 'jpg', 'json', 'json0', 'pct', 'pdf', 'pic', 'pict', 'plain', 'plain-ext', 'png', 'pov', 'ps', 'ps2', 'psd', 'sgi', 'svg', 'svgz', 'tga', 'tif', 'tiff', 'tk', 'vml', 'vmlz', 'vrml', 'wbmp', 'webp', 'x11', 'xdot', 'xdot1.2', 'xdot1.4', 'xdot_json', 'xlib'])
-            vis_path = os.path.join(app.config['GRAPH_FOLDER'],
-                                    filename.rsplit('.', 1)[0].lower() + ".png")
-            net, initial_marking, final_marking = mining_process_model(file_path)
-            pm4py.write_petri_net(net, initial_marking, final_marking, pnml_path)
-            pm4py.save_vis_petri_net(net, initial_marking, final_marking, vis_path)
-            # graphviz.render('dot', 'png', graph_path).replace('\\', '/')
-            return filename.rsplit(
-                '.', 1)[0].lower(), "The file has been successfully uploaded."
+    @app.route('/pm4pytest', methods=['GET'])
+    def pm4pytest():
+        filename = 'loan_process.xes'
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        if not os.path.exists(app.config['GRAPH_FOLDER']):
+                    os.makedirs(app.config['GRAPH_FOLDER'])
+        pnml_path = os.path.join(app.config['GRAPH_FOLDER'],
+                                filename.rsplit('.', 1)[0].lower() + ".pnml")
+        # (must be one of ['bmp', 'canon', 'cgimage', 'cmap', 'cmapx', 'cmapx_np', 'dot', 'dot_json', 'eps', 'exr', 'fig', 'gd', 'gd2', 'gif', 'gtk', 'gv', 'ico', 'imap', 'imap_np', 'ismap', 'jp2', 'jpe', 'jpeg', 'jpg', 'json', 'json0', 'pct', 'pdf', 'pic', 'pict', 'plain', 'plain-ext', 'png', 'pov', 'ps', 'ps2', 'psd', 'sgi', 'svg', 'svgz', 'tga', 'tif', 'tiff', 'tk', 'vml', 'vmlz', 'vrml', 'wbmp', 'webp', 'x11', 'xdot', 'xdot1.2', 'xdot1.4', 'xdot_json', 'xlib'])
+        vis_path = os.path.join(app.config['GRAPH_FOLDER'],
+                                filename.rsplit('.', 1)[0].lower() + ".png")
+        net, initial_marking, final_marking = mining_process_model(file_path)
+        pm4py.write_petri_net(net, initial_marking, final_marking, pnml_path)
+        pm4py.save_vis_petri_net(net, initial_marking, final_marking, vis_path)
+        # graphviz.render('dot', 'png', vis_path).replace('\\', '/')
+        return send_from_directory(app.config['GRAPH_FOLDER'], 'loan_process.png')
+
+
+    @app.route('/upload', methods=['GET', 'POST'])
+    def upload_file():
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                return "No file uploaded. Please select a file.", 400
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                return "No file is selected.", 400
+                # return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(file_path)
+                if not os.path.exists(app.config['GRAPH_FOLDER']):
+                    os.makedirs(app.config['GRAPH_FOLDER'])
+                pnml_path = os.path.join(app.config['GRAPH_FOLDER'],
+                                filename.rsplit('.', 1)[0].lower() + ".pnml")
+                # (must be one of ['bmp', 'canon', 'cgimage', 'cmap', 'cmapx', 'cmapx_np', 'dot', 'dot_json', 'eps', 'exr', 'fig', 'gd', 'gd2', 'gif', 'gtk', 'gv', 'ico', 'imap', 'imap_np', 'ismap', 'jp2', 'jpe', 'jpeg', 'jpg', 'json', 'json0', 'pct', 'pdf', 'pic', 'pict', 'plain', 'plain-ext', 'png', 'pov', 'ps', 'ps2', 'psd', 'sgi', 'svg', 'svgz', 'tga', 'tif', 'tiff', 'tk', 'vml', 'vmlz', 'vrml', 'wbmp', 'webp', 'x11', 'xdot', 'xdot1.2', 'xdot1.4', 'xdot_json', 'xlib'])
+                vis_path = os.path.join(app.config['GRAPH_FOLDER'],
+                                        filename.rsplit('.', 1)[0].lower() + ".png")
+                net, initial_marking, final_marking = mining_process_model(file_path)
+                pm4py.write_petri_net(net, initial_marking, final_marking, pnml_path)
+                pm4py.save_vis_petri_net(net, initial_marking, final_marking, vis_path)
+                # graphviz.render('dot', 'png', graph_path).replace('\\', '/')
+                return filename.rsplit(
+                    '.', 1)[0].lower(), "The file has been successfully uploaded."
+            else:
+                return "This file type is not allowed. Please select a XES file.", 406
         else:
-            return "This file type is not allowed. Please select a XES file.", 406
-    else:
-        if not os.path.exists(app.config['UPLOAD_FOLDER']):
-            os.makedirs(app.config['UPLOAD_FOLDER'])
-            return "no file uploaded yet", 404
-        dataSets = os.listdir(app.config['UPLOAD_FOLDER'])
-        if len(dataSets) == 0:
-            return "no file uploaded yet", 404
-        dataSets_dict = {}
-        i = 0
-        for dataSet in dataSets:
-            dataSets_dict[i] = dataSet
-            i += 1
-        return dataSets_dict, 200
+            if not os.path.exists(app.config['UPLOAD_FOLDER']):
+                os.makedirs(app.config['UPLOAD_FOLDER'])
+                return "no file uploaded yet", 404
+            dataSets = os.listdir(app.config['UPLOAD_FOLDER'])
+            if len(dataSets) == 0:
+                return "no file uploaded yet", 404
+            dataSets_dict = {}
+            i = 0
+            for dataSet in dataSets:
+                dataSets_dict[i] = dataSet
+                i += 1
+            return dataSets_dict, 200
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)

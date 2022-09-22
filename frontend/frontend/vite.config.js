96963b7a5e4861bdd70904c1bbc5ab88540f6e41
Fanxin-Yang <fanxin.yang@tum.de>
2022-09-22 13:37:17 +0200

    chaneg host, remove base

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ command, mode }) => {
   console.log(mode);
   if (command === "serve") {
     return {
-      base: "https://lehre.bpm.in.tum.de/ports/8051/",
+      // base: "https://lehre.bpm.in.tum.de/",
       // dev specific config
       plugins: [vue()],
       // resolve: {
@@ -16,7 +16,7 @@ export default defineConfig(({ command, mode }) => {
       //   },
       // },
       server: {
-        host: "::1",
+        host: "localhost",
         // port: 3000,
         // strictPort: true,
         // open: true,

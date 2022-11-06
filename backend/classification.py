f8f1f9fb52daa0bda8da0ba1efc5c019ed6b1471
Fanxin-Yang <fanxin.yang@tum.de>
2022-11-06 21:19:55 +0100

    apply JRip, get JRip description, exist_arff

--- backend/classification.py
+++ backend/classification.py
@@ -1,11 +1,13 @@
 import os
 from flask import request, current_app, send_file
 import matplotlib
-import pandas, numpy
+import pandas
+import numpy
 from sklearn import tree, preprocessing
 from dtreeviz.trees import dtreeviz
 from sklearn.compose import make_column_selector, make_column_transformer
 import discovery_algorithm
+
 
 def get_algorithms():
     algorithms = {}
@@ -31,6 +33,7 @@ def get_decisiontree(filename, csv):
         return "No decisiont tree found.", 404
     else:
         return send_file(svg_path)
+
 
 def get_decisionrule(filename, csv):
     csv_path = discovery_algorithm.exist_csv(filename, csv)
@@ -67,14 +70,20 @@ def filter(args, partial_log):
     # print(partial_log.head)
     return partial_log
 
+
 def encoder_X_y(encoder, X, y):
-    encoderX = preprocessing.OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1) if encoder == "0" else preprocessing.OneHotEncoder(handle_unknown='ignore')
+    if encoder == "0":
+        encoderX = preprocessing.OrdinalEncoder(
+            handle_unknown='use_encoded_value', unknown_value=-1)
+    else:
+        encoderX = preprocessing.OneHotEncoder(handle_unknown='ignore')
     # only encode the columns which are categorical varibales
     ct = make_column_transformer((encoderX, make_column_selector(
         dtype_include='category')), remainder='passthrough', verbose_feature_names_out=False)
     # data_train = ct.fit_transform(X_train)
     # data_test = ct.fit_transform(X_test)
-    data = ct.fit_transform(X) #if encoder == "0" else ct.fit_transform(X).toarray()
+    # if encoder == "0" else ct.fit_transform(X).toarray()
+    data = ct.fit_transform(X)
     feature_names = ct.get_feature_names_out()
     print(feature_names)
     encoderX.fit(X)
@@ -91,10 +100,12 @@ def encoder_X_y(encoder, X, y):
         #     y_train.values.reshape(-1, 1)).reshape(1, -1)[0]
         # target_test = encodery.fit_transform(
         #     y_test.values.reshape(-1, 1)).reshape(1, -1)[0]
-        target = encodery.fit_transform(y.values.reshape(-1, 1)).reshape(1, -1)[0]
+        target = encodery.fit_transform(
+            y.values.reshape(-1, 1)).reshape(1, -1)[0]
     target_names = y.unique()
     # tmp = y.values.reshape(-1, 1)
     return data, target, feature_names, target_names
+
 
 def visualize(clf, data, target, feature_names, target_names, svg_path):
     # dot_data = tree.export_graphviz(
@@ -102,7 +113,7 @@ def visualize(clf, data, target, feature_names, target_names, svg_path):
     #     special_characters=True, feature_names=feature_names, class_names=list(target_names))
     # graph = graphviz.Source(dot_data)
     # graph.render(csv + "_" + alg, results_folder, format="png")
-    matplotlib.pyplot.switch_backend('Agg')
+    matplotlib.pyplot.switch_backend('Agg')  # type: ignore
     viz = dtreeviz(clf, x_data=data, y_data=target,
                    target_name="class",
                    feature_names=feature_names,
@@ -124,10 +135,10 @@ def get_rules(clf, feature_names, class_names):
 
     paths = []
     path = []
-    
+
     def recurse(node, path, paths):
-        
-        if tree_.feature[node] != tree._tree.TREE_UNDEFINED:
+
+        if tree_.feature[node] != tree._tree.TREE_UNDEFINED:  # type: ignore
             name = feature_name[node]
             threshold = tree_.threshold[node]
             p1, p2 = list(path), list(path)
@@ -138,37 +149,33 @@ def get_rules(clf, feature_names, class_names):
         else:
             path += [(tree_.value[node], tree_.n_node_samples[node])]
             paths += [path]
-            
+
     recurse(0, path, paths)
 
     # sort by samples count
     samples_count = [p[-1][1] for p in paths]
     ii = list(numpy.argsort(samples_count))
     paths = [paths[i] for i in reversed(ii)]
-    
+
     rules = []
     for path in paths:
         rule = "if "
-        
+
         for p in path[:-1]:
             if rule != "if ":
                 rule += " and "
             rule += str(p)
         rule += " then "
         if class_names is None:
-            rule += "response: "+str(numpy.round(path[-1][0][0][0],3))
+            rule += "response: "+str(numpy.round(path[-1][0][0][0], 3))
         else:
             classes = path[-1][0][0]
             l = numpy.argmax(classes)
             rule += f"class: {class_names[l]} (proba: {numpy.round(100.0*classes[l]/numpy.sum(classes),2)}%)"
         rule += f" | based on {path[-1][1]:,} samples"
         rules += [rule]
-        
+
     return rules
-
-
-
-
 
 
 def apply_algorithm(filename, csv, alg):
@@ -214,14 +221,15 @@ def apply_algorithm(filename, csv, alg):
         # options = cost_complexity_pruning(data, target, clf)
     else:
         ccp_alpha = float(ccp_alpha)
-        clf = tree.DecisionTreeClassifier(criterion="gini", splitter="random", ccp_alpha=ccp_alpha)
-    options = cost_complexity_pruning(data, target, clf)    
-    test_target = y.values.reshape(-1, 1)
+        clf = tree.DecisionTreeClassifier(
+            criterion="gini", splitter="random", ccp_alpha=ccp_alpha)
+    options = cost_complexity_pruning(data, target, clf)
     clf = clf.fit(data, target)
-    if clf.tree_.node_count > 100: 
-        if ccp_alpha == -1: return options, 202
-        else: 
-            # options bigger than ccp_alpha 
+    if clf.tree_.node_count > 100:
+        if ccp_alpha == -1:
+            return options, 202
+        else:
+            # options bigger than ccp_alpha
             greaterOptions = {}
             i = 0
             for j in range(len(options)):
@@ -230,12 +238,11 @@ def apply_algorithm(filename, csv, alg):
                     i += 1
             return greaterOptions, 202
 
-
     results_folder = os.path.join(
         current_app.config['RESULTS_FOLDER'], filename)
     if not os.path.exists(results_folder):
         os.makedirs(results_folder)
-        
+
     rule_path = os.path.join(results_folder, csv + "_" + "decisionrule.txt")
     rules = get_rules(clf, feature_names, target_names)
     with open(rule_path, "w+") as f:
@@ -249,8 +256,6 @@ def apply_algorithm(filename, csv, alg):
         return options, 201
     else:
         return f"With selected ccp_alpha value {ccp_alpha} decision tree has {clf.tree_.node_count} nodes and max depth {clf.tree_.max_depth}.", 200
-
-    # append ISC candidates
 
 
 def cost_complexity_pruning(data, target, clf):
@@ -318,3 +323,112 @@ def cost_complexity_pruning(data, target, clf):
         options[index] = ccp_alphas[i]
         index += 1
     return options
+
+
+def apply_algorithm_weka(filename, arff, alg):
+    args = request.args.copy()
+    cls_options = args.pop("cls_options", "")
+    arff_path = exist_arff(filename, arff)
+    if not arff_path:
+        return "No file found.", 404
+    results_folder = os.path.join(
+        current_app.config['RESULTS_FOLDER'], filename)
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
+    result_path = os.path.join(results_folder, arff + "_" + alg)
+
+    if alg == "JRip":
+        return apply_JRip(arff_path, cls_options, result_path)
+    # for other algorithms
+    return "No support for this algorithm yet."
+
+
+def apply_JRip(arff_path, cls_options, result_path):
+    cls_cmdline = "weka.classifiers.rules.JRip " + cls_options
+    result_path += ".txt"
+
+    import weka.core.jvm as jvm
+    jvm.start(max_heap_size="512m", system_cp=True)
+    print("----------------------------------------------------------------------")
+    import weka.core.converters as converters
+    loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+    data = loader.load_file(arff_path, class_index="first")
+    # usually the first is "concept:name"
+    # the class label will not be removed by weka.filters.unsupervised.attribute.*
+    print(data.attribute_names(), data.num_instances)
+
+    # from weka.classifiers import Classifier
+    from weka.core.classes import to_commandline, from_commandline
+    # cls = Classifier(classname="weka.classifiers.rules.JRip",
+    #                  options=["-N", "5.0"])
+    # cmdline = 'weka.classifiers.rules.JRip -F 3 -N 5.0 -O 2 -S 1'
+    print(cls_cmdline)
+    cls = from_commandline(
+        cls_cmdline, classname="weka.classifiers.Classifier")
+
+    # Filter string because JRip cannot handle string
+    from weka.filters import Filter
+    remove = Filter(
+        classname="weka.filters.unsupervised.attribute.RemoveType", options=["-T", "string"])
+    remove.inputformat(data)
+    filtered_data = remove.filter(data)
+    print(f"Attributes after filter: {filtered_data.attribute_names()}")
+
+    from javabridge.jutil import JavaException
+    try:
+        cls.build_classifier(filtered_data)  # type: ignore
+    except JavaException:
+        return "JRip cannot handle string class! Since string attributes have been removed. The class label is string type"
+
+    # save rule set in txt file
+    print(result_path)
+    with open(result_path, "w+") as f:
+        for r in str(cls).split("\n"):
+            f.write(r + "\n")
+    jvm.stop()
+
+    return "The rule set by applying JRip is saved."
+
+
+def exist_arff(filename, arff):
+    arff_path = os.path.join(
+        current_app.config['OUTPUT_FOLDER'], filename + "_" + arff + ".arff")
+    if os.path.exists(arff_path):
+        return arff_path
+    if len(arff.split("&")) == 1 and len(filename.split("&")) > 1:
+        for log in filename.split("&"):
+            folder = os.path.join(
+                current_app.config['OUTPUT_FOLDER'], log)
+            if arff.rsplit("_", 1)[1] == "modified":
+                arff_folder = os.path.join(folder, arff.rsplit("_", 1)[0])
+            else:
+                arff_folder = os.path.join(folder, arff)
+            arff_path = os.path.join(arff_folder, arff + '.arff')
+            if os.path.exists(arff_path):
+                return arff_path
+    folder = os.path.join(
+        current_app.config['OUTPUT_FOLDER'], filename)
+    if len(arff.split("_")) > 1:
+        if arff.rsplit("_", 1)[1] == "modified":
+            arff_folder = os.path.join(folder, arff.rsplit("_", 1)[0])
+        else:
+            arff_folder = os.path.join(folder, arff)
+    else:
+        arff_folder = os.path.join(folder, arff)
+    arff_path = os.path.join(arff_folder, arff + '.arff')
+    if not os.path.exists(arff_path):
+        return False
+    else:
+        return arff_path
+
+
+def get_alg_description(alg):
+    if alg == "JRip":
+        import weka.core.jvm as jvm
+        jvm.start(max_heap_size="512m", system_cp=True)
+        from weka.classifiers import Classifier
+        cls = Classifier(classname="weka.classifiers.rules.JRip",
+                         options=["-N", "5.0"])
+        description = cls.description()
+        jvm.stop()
+        return description
